471e603beb3f274a3765e3ac0c09d54d45389169
Yue Wu <yue.wu@yale.edu>
2025-05-06 14:41:15 -0400

    add initial results of capacity rsc code

---
+++ accuracy/capacity-rsc/run.py
@@ -0,0 +1,54 @@
+import os
+import arguably
+import multiprocessing
+
+local_maximum_jobs: int = multiprocessing.cpu_count()
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+notebook_filepath = os.path.join(this_dir, "capacity-rsc.ipynb")
+
+decoder_vec = ["fb(max_tree_size=0)", "fb", "mwpf(c=0)", "mwpf(c=50)"]
+p_d_vec = [
+    ("0.4", [3, 5, 7, 9, 11, 13]),
+    ("0.3", [3, 5, 7, 9, 11, 13]),
+    ("0.2", [3, 5, 7, 9, 11, 13]),
+    ("0.15", [3, 5, 7, 9, 11, 13]),
+    ("0.14", [3, 5, 7, 9, 11, 13]),
+    ("0.13", [3, 5, 7, 9, 11, 13]),
+    ("0.12", [3, 5, 7, 9, 11, 13]),
+    ("0.1", [3, 5, 7, 9, 11, 13]),
+    ("0.05", [3, 5, 7, 9, 11, 13]),
+    ("0.02", [3, 5, 7, 9, 11]),
+    ("0.01", [3, 5, 7]),
+    ("0.005", [3, 5]),
+    ("0.002", [3]),
+    ("0.001", [3]),
+]
+
+code_vec: list[str] = []
+noise_vec: list[str] = []
+for p, d_vec in p_d_vec:
+    for d in d_vec:
+        code_vec.append(f"css_rsc(d={d})")
+        noise_vec.append(f"depolarize(p={p})")
+
+
+@arguably.command
+def main(target_precision: float = 0.04):
+    from qec_lego_bench.notebooks.pL_p_compare_decoders import (
+        notebook_pL_p_compare_decoders,
+    )
+
+    notebook_pL_p_compare_decoders(
+        notebook_filepath=notebook_filepath,
+        code=code_vec,
+        noise=noise_vec,
+        decoder=decoder_vec,
+        target_precision=target_precision,
+        local_maximum_jobs=local_maximum_jobs - 1,
+        max_shots=10_000_000,
+    )
+
+
+if __name__ == "__main__":
+    arguably.run()
