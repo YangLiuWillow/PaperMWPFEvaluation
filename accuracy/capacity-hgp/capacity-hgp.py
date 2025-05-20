84519452902254263b4f15203aa403586440bb42
Yue Wu <yue.wu@yale.edu>
2025-05-19 22:04:44 -0400

    update data

--- accuracy/capacity-hgp/capacity-hgp.py
+++ accuracy/capacity-hgp/capacity-hgp.py
@@ -8,40 +8,31 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 assert os.path.abspath(__file__)[-3:] == ".py"
 notebook_filepath = os.path.abspath(__file__)[:-3] + ".ipynb"
 
-code_dict: dict[int, str] = {
-    72: "bb(n=72,k=12,d=6)",
-    90: "bb(n=90,k=8,d=10)",
-    108: "bb(n=108,k=8,d=10)",
-    144: "bb(n=144,k=12,d=12)",
-    288: "bb(n=288,k=12,d=18)",
-}
 
-decoder_vec = ["mwpf(c=0)", "mwpf(c=200)"]
-p_d_vec = [
-    ("0.4", [72, 90, 144]),
-    ("0.3", [72, 90, 144]),
-    ("0.2", [72, 90, 144]),
-    ("0.1", [72, 90, 144]),
-    ("0.05", [72, 90, 144]),
-    ("0.04", [72, 90, 144]),
-    ("0.03", [72, 90, 144]),
-    ("0.02", [72, 90, 144]),
-    ("0.015", [72, 90, 144]),
-    ("0.01", [72, 90]),
-    ("0.008", [72]),
-    ("0.005", [72]),
+decoder_vec = ["mwpf(c=0)", "mwpf(c=200)", "bposd(max_iter=4,ms_scaling_factor=0.3)"]
+p_vec = [
+    "0.2",
+    "0.1",
+    "0.05",
+    "0.04",
+    "0.03",
+    "0.025",
+    "0.02",
+    "0.018",
+    "0.015",
+    "0.0125",
+    "0.01",
 ]
 
 code_vec: list[str] = []
 noise_vec: list[str] = []
-for p, n_vec in p_d_vec:
-    for n in n_vec:
-        code_vec.append(code_dict[n])
-        noise_vec.append(f"depolarize(p={p})")
+for p in p_vec:
+    code_vec.append("hgp")
+    noise_vec.append(f"depolarize(p={p})")
 
 
 @arguably.command
-def main(*, target_precision: float = 0.04):
+def main(*, target_precision: float = 0.04, slurm_maximum_jobs: int = 100):
     from qec_lego_bench.notebooks.pL_p_compare_decoders import (
         notebook_pL_p_compare_decoders,
     )
@@ -54,6 +45,7 @@ def main(*, target_precision: float = 0.04):
         target_precision=target_precision,
         local_maximum_jobs=local_maximum_jobs - 1,
         max_shots=10_000_000,
+        slurm_maximum_jobs=slurm_maximum_jobs,
     )
 
 
